3554073201259805ebe2eb3e704d8a49b33ed0f7
Lucas Carro <rylcarro@gmail.com>
2023-05-03 11:32:54 -0300

    feat_ejercicio23

--- Ejercicio_7/index.js
+++ Ejercicio_7/index.js
@@ -264,14 +264,245 @@ que aparece la letra "a":
 // console.log(multiplicarArreglo([1, 2, 3, 4, 5, 6, 7, 8])); // 40320
 // console.log(multiplicarArreglo([])); // 1
 
-/*Escribir una función llamada `removerCeros` que reciba un arreglo de números y retorne un nuevo 
-arreglo excluyendo los ceros (0).
+// /*Escribir una función llamada `removerCeros` que reciba un arreglo de números y retorne un nuevo
+// arreglo excluyendo los ceros (0).
+// */
+
+// function removerCeros(zeroArr) {
+//   return zeroArr.filter((xArr) => xArr > 0);
+// }
+
+// console.log(removerCeros([0, 1, 0, 2, 0, 3])); // [1, 2, 3]
+// console.log(removerCeros([9, 3, 6, 4])); // [9, 3, 6, 4]
+// console.log(removerCeros([0, 0, 0])); // []
+
+// /*
+// 14 - Escribir una función llamada `sumarArreglo` que reciba tres argumentos: un arreglo de números,
+// la posición inicial y la posición final. La función debe retornar la suma de todos los números
+// dentro del rango (la posición inicial y la posición final, incluyéndolas).
+
+// **Nota:** puedes asumir que la posición inicial va a ser menor o igual a la posición final, y
+// que están dentro de los límites del arreglo.
+// */
+
+// function sumarArreglo(arrNum, initPos, finalPos) {
+//   let result = 0;
+
+//   for (i = initPos; i <= finalPos; i++) {
+//     result += arrNum[i];
+//   }
+
+//   return result;
+// }
+
+// // código de prueba
+// console.log(sumarArreglo([1, 2, 3], 1, 2)); // 5
+// console.log(sumarArreglo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3, 6)); // 22
+// console.log(sumarArreglo([7, 8, 9], 0, 0)); // 0
+
+// /*
+// 15 - Escribir una función llamada `transcribir` que reciba un string (una cadena de ADN) y retorne otro string
+// (su complemento ARN).
+
+// Los complementos son los siguientes:
+
+// - G -> C
+// - C -> G
+// - T -> A
+// - A -> U
+// */
+
+// function transcribir(ADNchain) {
+//   let ARN = [""];
+//   for (i = 0; i < ADNchain.length; i++) {
+//     if (ADNchain[i] == "G") {
+//       ARN.push("C");
+//     } else if (ADNchain[i] == "C") {
+//       ARN.push("G");
+//     } else if (ADNchain[i] == "T") {
+//       ARN.push("A");
+//     } else {
+//       ARN.push("U");
+//     }
+//   }
+
+//   return ARN.join("");
+// }
+
+// // código de prueba
+// console.log(transcribir("ACGT")); // "UGCA"
+// console.log(transcribir("ACGTGGTCTTAA")); // "UGCACCAGAAUU"
+
+// /*
+// 16 - Escribir una función llamada `capitalizar` que reciba un string y capitalice la primera letra:
+// */
+
+// function capitalizar(toCap){
+//   return toCap.charAt(0).toUpperCase() + toCap.slice(1)
+// }
+
+// // código de prueba
+// console.log(capitalizar("pedro")); // "Pedro"
+// console.log(capitalizar("hola mundo")); // "Hola mundo"
+// console.log(capitalizar("")); // ""
+
+// /*
+// 17 - Escribir una función llamada `capitalizar` que reciba un string
+// y capitalice la primera letra **de cada palabra**:
+// */
+
+// function capitalizar(toCapAll) {
+//   return toCapAll
+//     .split(" ")
+//     .map(function (toCapAll) {
+//       return toCapAll.charAt(0).toUpperCase() + toCapAll.slice(1);
+//     })
+//     .join(" ");
+// }
+
+// // código de prueba
+// console.log(capitalizar("hola mundo")); // "Hola Mundo"
+// console.log(capitalizar("make it real")); // "Make It Real"
+// console.log(capitalizar("")); // ""
+
+// /*
+// 18 - Escribir una función llamada `max` que reciba un arreglo de números y retorne el número máximo:
+
+// **Nota:** Intentarlo hacer sin el método `Math.max` de JavaScript.
+// */
+
+// function max(arrMax) {
+//   let maxValue = arrMax[0];
+
+//   for (i = 0; i < arrMax.length; i++) {
+//     if (arrMax[i] > maxValue) {
+//       maxValue = arrMax[i];
+//     }
+//   }
+
+//   return maxValue;
+// }
+
+// // código de prueba
+// console.log(max([3, 9, 6])); // 9
+// console.log(max([67, 35, 54, 26])); // 67
+// console.log(max([5, 9, 2, 4, 5, 7])); // 9
+
+// /*
+// 19 - Escribir una función llamada `min` que reciba un arreglo de números y retorne el número mínimo:
+
+// **Nota:** Intentarlo hacer sin el método `Math.min` de JavaScript.
+// */
+
+// function min(arrMin) {
+//   let minValue = arrMin[0];
+
+//   for (i = 0; i < arrMin.length; i++) {
+//     if (arrMin[i] < minValue) {
+//       minValue = arrMin[i];
+//     }
+//   }
+
+//   return minValue;
+// }
+
+// // código de prueba
+// console.log(min([3, 9, 6])); // 3
+// console.log(min([67, 35, 54, 26])); // 26
+// console.log(min([5, 9, 2, 4, 5, 7])); // 2
+
+// /*
+// 20 - Escribir una función llamada `password` que reciba un string y retorne un nuevo string realizando los siguientes cambios:
+
+// - Las mayúsculas se reemplazan por minúsculas.
+// - Se eliminan los espacios en blanco.
+// - Reemplaza el caracter “a” por “4”.
+// - Reemplaza el caracter “e” por “3”.
+// - Reemplaza el caracter “i” por “1”.
+// - Reemplaza el carater “o” por “0”.
+// */
+
+// function password(pwd){
+//   let pwdjoined = pwd.split('').join('')
+//   let pwdCased = []
+//   let pwdFinal = []
+
+//     for(i = 0; i < pwdjoined.length; i++){
+//       if(/[A-Z]/.test(pwdjoined[i]) === true){
+//         pwdCased.push(pwdjoined[i].toLowerCase())
+//       } else {
+//         pwdCased.push(pwdjoined[i])
+//       }
+//     }
+
+//     for(i = 0; i < pwdCased.length; i++){
+//       switch(pwdCased[i]){
+//         case 'a': pwdFinal.push('4')
+//           break;
+//         case 'e': pwdFinal.push('3')
+//           break;
+//         case 'i': pwdFinal.push('1')
+//           break;
+//         case 'o': pwdFinal.push('0')
+//           break;
+//         default: pwdFinal.push(pwdCased[i])
+//       }
+//     }
+
+//     return pwdFinal.join('')
+
+// }
+
+// // código de prueba
+// console.log(password("hola")); // "h0l4"
+// console.log(password("esta es una prueba")); // "3st43sun4pru3b4"
+// console.log(password("")); // ""
+
+// /*
+// 21 - Escribir una función llamada `pares` que reciba un arreglo de números y retorne un nuevo arreglo
+// con los números pares únicamente.
+// */
+
+// function pares(arrNotEvens) {
+
+//   let arrEvens = [];
+
+//   for (i = 0; i < arrNotEvens.length; i++) {
+//     if (arrNotEvens[i] % 2 == 0) {
+//       arrEvens.push(arrNotEvens[i]);
+//     }
+//   }
+
+//   return arrEvens;
+// }
+
+// // código de prueba
+// console.log(pares([1, 2, 3, 4, 5, 6])); // [2, 4, 6]
+// console.log(pares([])); // []
+
+/*
+22 - Escribir una función llamada `posiciones` que reciba un arreglo de números y retorne un nuevo arreglo **con las posiciones** 
+donde se encuentran números pares.
 */
 
-function removerCeros(zeroArr) {
-  return zeroArr.filter((xArr) => xArr > 0);
+function posiciones(arrPos) {
+  let positions = [];
+
+  for (i = 0; i < arrPos.length; i++) {
+    if (arrPos[i] % 2 === 0) {
+      positions.push(i);
+    }
+  }
+
+  return positions;
 }
 
-console.log(removerCeros([0, 1, 0, 2, 0, 3])); // [1, 2, 3]
-console.log(removerCeros([9, 3, 6, 4])); // [9, 3, 6, 4]
-console.log(removerCeros([0, 0, 0])); // []
+console.log(posiciones([1, 2, 3, 4, 5, 6])); // [1, 3, 5]
+console.log(posiciones([])); // []
+
+
+/*
+23 - Escribir una función llamada `duplicar` que reciba un arreglo de números y
+retorne un nuevo arreglo donde cada número esté multiplicado por dos (2).
+*/
+
